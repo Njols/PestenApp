c0664868af57865e3775f8a934190dc413c6a28c
Niels Boelens <412124@student.fontys.nl>
2019-02-26 13:42:51 +0100

    Added the option to use extra rules that are not related to individual cards

--- PestApp/Models/RuleSet.cs
+++ PestApp/Models/RuleSet.cs
@@ -5,10 +5,16 @@ using System.Threading.Tasks;
 
 namespace PestApp.Models
 {
+    public enum extraRules
+    {
+        takeAndPut, takeStacks, noPlayAfterTaking, endOnPestCard, stackAfterTake
+    }
     public class RuleSet
     {
         private int id;
         public int Id { get { return id; } }
+        private List<extraRules> extraRules;
+        public List<extraRules> ExtraRules {  get { return extraRules; } }
         private User _user;
         public User User { get { return _user; } }
         private List<Rule> _rules;
