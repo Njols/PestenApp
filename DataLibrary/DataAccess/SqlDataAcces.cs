98c3c70b3ed07c31d70435a4095a9cd9c307d435
Niels Boelens <412124@student.fontys.nl>
2019-03-13 10:38:44 +0100

    added data acces layer logic

---
+++ DataLibrary/DataAccess/SqlDataAcces.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using Dapper;
+
+namespace DataLibrary.DataAccess
+{
+    public static class SqlDataAcces
+    {
+        public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public static List<dynamic> LoadData(string query)
+        {
+            using (IDbConnection cnn = new SqlConnection(ConnectionString))
+            {
+                return cnn.Query(query).AsList();
+            }
+        }
+        public static int SaveData<T> (string query, T data)
+        {
+            using (IDbConnection cnn = new SqlConnection(ConnectionString))
+            {
+                return cnn.Execute(query, data);
+            }
+        }
+    }
+}
