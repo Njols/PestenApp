6e4dda9513d07ba8ae92d8c6759cd376010a0c03
Niels <niels@tonsoftime.com>
2019-03-20 13:37:20 +0100

    Business Logic

---
+++ DataLibrary/BusinessLogic/UserProcessor.cs
@@ -0,0 +1,28 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLibrary.BusinessLogic
+{
+    public static class UserProcessor
+    {
+        public static int CreateUser (string Email, string Username, string Password)
+        {
+            User user = new User
+            {
+                Email = Email,
+                Username = Username,
+                Password = Password
+            };
+            string sql = @"INSERT INTO dbo.User (Email, Username, Password) VALUES @Email, @Username, @Password";
+            return SqlDataAcces.SaveData(sql, user);
+        }
+        public static List<User> GetUsers ()
+        {
+            string sql = @"SELECT Id, Username, Email, Password FROM dbo.User";
+            return SqlDataAcces.LoadData<User>(sql);
+        }
+    }
+}
--- DataLibrary/DataAccess/SqlDataAcces.cs
+++ DataLibrary/DataAccess/SqlDataAcces.cs
@@ -10,11 +10,11 @@ namespace DataLibrary.DataAccess
     public static class SqlDataAcces
     {
         public static string ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        public static List<dynamic> LoadData(string query)
+        public static List<T> LoadData<T>(string query)
         {
             using (IDbConnection cnn = new SqlConnection(ConnectionString))
             {
-                return cnn.Query(query).AsList();
+                return cnn.Query<T>(query).AsList();
             }
         }
         public static int SaveData<T> (string query, T data)
