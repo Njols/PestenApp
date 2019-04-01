2dd7825f282688c782447e9c43909c8b4a083da8
Niels <niels@tonsoftime.com>
2019-04-01 10:25:29 +0200

    changed the add rule button to now show the rule and card you added in as strings

---
+++ PestApp/wwwroot/js/main.js
@@ -0,0 +1,21 @@
+﻿var cardSelection = document.getElementById("card-selection");
+var ruleSelection = document.getElementById("rule-selection");
+var addRuleButton = document.getElementById("addRuleButton");
+var selectContainer = document.getElementById("select-container");
+
+addRuleButton.onclick = CreateNewElement;
+
+function moveSelectContainer() {
+    var bottomNumber = selectContainer.style.bottom.replace('px', '');
+    var bottom = parseInt(bottomNumber, 10);
+    selectContainer.style.bottom = '${bottom + 25}px';
+}
+function CreateNewElement() {
+    var para = document.createElement("p");
+    var card = cardSelection.value;
+    var rule = ruleSelection.value;
+    var node = document.createTextNode(card + " " + rule);
+    para.appendChild(node);
+
+    selectContainer.appendChild(para);
+}
